d2da54190e1dcc72c52e922505e48cf3acf5a5ad
Jay Panchal <138220900+JayPanchalEZ@users.noreply.github.com>
2024-09-01 06:14:58 +0000

    2 files added

---
+++ 01_basics/1_variables.js
@@ -0,0 +1,12 @@
+const accId = 123
+let wow = "Jay Boy"
+var bruh = "oops"
+let notDefined;
+console.log(wow);
+//accId = 2 Not allowed
+wow = "Jay Panchal"
+bruh = "Oh yeah"
+
+
+
+console.table([accId, wow, bruh, notDefined]);
---
+++ 01_basics/2_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version of JS
+
+//alert(3+3)  //we're using nodejs not browser so it'll not work here..
+
+//code readability should be high
+
+let name = "Jay"  //String
+let age = 20 //Integer
+let isLoggedIn = false //boolean
+
+// number => 2 to pwer 53
+// bigint
+// String => ""
+// Boolean => true/false
+// null (a standalone value or type) representation of empty value, here we're assigning value as null
+// undefined means no value or no datatype assigned to that variable
+// symbol => to find uniqueness/ unique
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
